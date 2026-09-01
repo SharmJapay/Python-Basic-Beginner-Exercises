6e74906bfbecbfc5f4794c9b85618c268ce71903
Sharm Japay <sharmainejoyjapay.07@gmail.com>
2026-09-01 23:07:42 +0800

    Add code for exercise 'Cumulative Sum of a Range'

---
+++ 2 Cumulative Sum of a Range/main.py
@@ -0,0 +1,18 @@
+"""Solution for Cumulative Sum of a Range"""
+
+
+def main() -> None:
+    """Starts the program and executes the applications flow"""
+
+    prev_number = 0
+
+    for number in range(10):
+        print(
+            f"Current Number {number} Previous Number {prev_number} Sum: {number + prev_number}"
+        )
+
+        prev_number = number
+
+
+if __name__ == "__main__":
+    main()
